9293a916c7f331a6b5de35591d97e135bb9f94df
Ddg80 <dguillin80@gmail.com>
2024-03-27 21:28:30 +0100

    feat(SearchBar): How to create Emit event from child to parent

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,2 +1,33 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { ref } from "vue";
+const emit = defineEmits(["search"]);
+const searchInput = ref("");
+
+const searchMovie = () => {
+  console.log(searchInput.value);
+  // Émettre le terme de recherche vers le composant parent
+  emit("search", searchInput.value);
+};
+</script>
+
+<template>
+  <section class="search-section bg-sky-50 py-4">
+    <div class="container mx-auto md:px-40 px-5">
+      <div class="flex">
+        <input
+          class="border border-sky-400 py-2 px-4 w-full rounded-l-full focus:outline-none focus:ring-2 focus:ring-sky-400"
+          type="text"
+          placeholder="Rechercher un film..."
+          v-model="searchInput"
+        />
+        <button
+          class="bg-sky-500 hover:bg-sky-600 text-white py-4 px-4 rounded-r-full"
+          type="submit"
+          @click="searchMovie()"
+        >
+          Rechercher
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,16 @@
 <script setup>
-import { ref } from "vue";
-const searchInput = ref("");
-const searchMovie = () => {
-  console.log(searchInput.value);
+import SearchBar from "../components/SearchBar.vue";
+
+const searchTerm = ref("");
+
+const onSearch = () => {
+  console.log(`Terme de recherche du composant enfant : ${searchTerm.value}`);
+  // Vous pouvez potentiellement effectuer une recherche réelle ici en utilisant searchTerm
 };
 </script>
+
 <template>
-  <section class="search-section bg-sky-50 py-4">
-    <div class="container mx-auto md:px-40 px-5">
-      <div class="flex">
-        <input
-          class="border border-sky-400 py-2 px-4 w-full rounded-l-full focus:outline-none focus:ring-2 focus:ring-sky-400"
-          type="text"
-          placeholder="Search Movie..."
-          v-model="searchInput"
-        />
-        <button
-          class="bg-sky-500 hover:bg-sky-600 text-white py-4 px-4 rounded-r-full"
-          type="submit"
-          @click="searchMovie()"
-        >
-          Search
-        </button>
-      </div>
-    </div>
-  </section>
+  <div>
+    <SearchBar v-model="searchTerm" @search="onSearch" />
+  </div>
 </template>
